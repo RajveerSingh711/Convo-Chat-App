d506b341404665a2d80c27edae03ad1c6b7837ac
Rajveer <122341523+RajveerSingh711@users.noreply.github.com>
2024-11-23 19:12:02 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+const allowedOrigins = ["http://localhost:5173"]; 
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true })); 
+
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+server.listen(PORT, () => {
+  console.log(`Server is running in development mode on PORT: ${PORT}`);
+  connectDB();
+});
